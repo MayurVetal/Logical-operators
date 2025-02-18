cdbf2c31a000b3768df78f078e6c487f2f565db4
MayurVetal <155822296+MayurVetal@users.noreply.github.com>
2025-02-18 12:56:30 +0530

    Add files via upload

---
+++ Assignment operators.py
@@ -0,0 +1,73 @@
+#ASSIGNMENT OPERATORS IN PYTHON
+
+
+
+x=10
+y=5
+
+
+#Addition assignmnet
+x+=5
+print(f"x after addition assignment:{x}")
+
+
+#Substraction Assignment
+x-=3
+print(f"x after substraction assignment:{x}")
+
+
+#Multiple Assignment
+x*=2
+print(f"x after multiplication assignment:{x}")
+
+
+#Division Assignment
+x/=4
+print(f"x after division assignment:{x}")
+
+
+#Floor division assignment
+x//=2
+print(f"x after floor division assignment:{x}")
+
+
+#Moduls Assignment
+x%=3
+print(f"After moduls assignment:{x}")
+
+
+#Exponentition operators
+x**=2
+print(f"x after expontition assignment{x}")
+
+
+#Bitwise AND assignment
+x&=1
+print(f"x after AND assignment{x}")
+
+
+#Bitwise OR operators
+x|=3
+print(f"x after OR operators{x}")
+
+
+#bitwise XOR operators
+x^=2
+print(f"x after XOR operators:{x}")
+
+
+
+#Bitwise rught shift operators assignment
+x>>=1
+print(f"x after bitwise right shift operators :{x}")
+
+
+#Bitwise left shift operators
+x<<=2
+print(f"x after left shift ioperatirs :{x}")
+
+
+
+#Use of multiple assignment in one line
+a,b,c=1,2,3
+print(f"Multiple assignment:a={a},b={b},c={c}")
