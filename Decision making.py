6e2ac534aa86d90c92b175558dbb03f6cb02baf6
MayurVetal <155822296+MayurVetal@users.noreply.github.com>
2025-02-18 13:16:59 +0530

    Add files via upload

---
+++ Decision making.py
@@ -0,0 +1,40 @@
+#DECISION MAKING CONDITION STATEMENTS
+
+
+#1. IF STATEMENT
+a=100
+b=160
+if b>a:
+    print("b is greater than a")
+
+
+
+
+#2.IF-ELSE-STATEMENT
+a=100
+b=160
+if b>a:
+    print("b is greater than a")
+else:
+    print("a is greater than b")
+
+
+#3.IF..ELIF..ELSE STATEMENT
+score=85
+if score>=90:
+ print("Grade:A")
+elif score>=80:
+ print("Grade:B")
+elif score>=70:
+ print("Grade:C")
+else:
+ print("Grade:F")
+
+#4.NESTED IF-ELSE STATEMENT:
+num=10
+if num>0:
+    print("Number is positive.")
+if num %2==0:
+    print("Number is even.")
+else:
+    print("Number is odd.")
